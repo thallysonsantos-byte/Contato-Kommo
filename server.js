3fabb4ca5a0ef0646437a4e8fb70bd16c38641ea
Thallyson Correa Dos Santos <thallyson_santos@ceapema.matriz>
2025-10-15 14:13:17 -0300

    fix: corrigindo erro no tratamento de response

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 // Middleware para analisar corpos de requisição JSON
 app.use(express.json());
